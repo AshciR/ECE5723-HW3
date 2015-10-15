9a4b130cfcd61bdfc285efbf1559059b4cd4140f
Richard Walker <richard.walker.90@gmail.com>
2015-10-15 17:47:16 -0400

    Initial Commit

---
+++ ECE5723-HW3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ECE5723-HW3
+//
+//  Created by Richard Walker on 10/15/15.
+//  Copyright © 2015 Richard Walker. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
